63e3ab53d532b5bfee221b52fabea8cc86fb52c4
Guido Schmidt <git@guidoschmidt.cc>
2020-11-15 21:50:27 +0100

    EDIT/src/js: Logo.js react component

--- src/js/logo.js
+++ src/js/logo.js
@@ -1,213 +1,209 @@
-let canvas;
-let song;
-let fft;
-let mic;
-let sizeX;
-let center = 0;
-let binSize = 1024;
-let spectrum;
-let spectrumAverages;
-let logAverages;
-let octaveBands;
-let buttonSong;
-let buttonMic;
-let brandcolor;
-let backgroundColor;
-let fadeIn = 0;
-let circleCenterRadius;
-let innerRadius;
-let outerRadius;
-let points;
-let poissonWidth;
+// node_modules imports
+import React, { createRef } from "react";
+import ReactDOM from "react-dom";
+import p5 from "p5";
+import PoissonDiskSampling from "poisson-disk-sampling";
+// Local imports
+import "../p5/p5.sound.min.js";
+// Style imports
+import "../sass/logo.sass";
 
-function setupParameters(radius = 15) {
-  poissonWidth = 500;
-  innerRadius = radius;
-  outerRadius = innerRadius * 10;
-  let p = new PoissonDiskSampling({
-    shape: [poissonWidth, poissonWidth],
-    minDistance: innerRadius,
-    maxDistance: outerRadius,
-    tries: 5
-  });
-  points = p.fill();
-}
+class Logo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.sketchRef = createRef();
+  }
 
-function preload() {
-  const wrapper = document.querySelector("#logo-canvas-wrapper");
-  const songURL = wrapper.getAttribute("data-song");
-  if (songURL === "") {
-    // const buttonSong = document.querySelector("#button-song");
-    // const buttonSongCookie = document.querySelector("#button-song-cookie");
-    // buttonSong.style.display = "none";
-    // buttonSongCookie.style.display = "none";
-  } else {
-    song = loadSound(songURL);
+  Sketch(p) {
+    let canvas;
+    let brandColor;
+    let backgroundColor;
+
+    let center = 0;
+    let circleCenterRadius;
+    let innerRadius;
+    let outerRadius;
+
+    let fadeIn = 0;
+    let poissonWidth;
+    let sizeX;
+
+    let points;
+
+    let fft;
+    let spectrum;
+    let spectrumAverages;
+    let mic;
+
+    let octaveBands;
+
+    let song;
+
+    p.stopAudio = () => {
+      song.stop();
+    };
+
+    const setupParameters = (radius = 15) => {
+      poissonWidth = 300;
+      innerRadius = radius;
+      outerRadius = innerRadius * 10;
+      let p = new PoissonDiskSampling({
+        shape: [poissonWidth, poissonWidth],
+        minDistance: innerRadius,
+        maxDistance: outerRadius,
+        tries: 5
+      });
+      points = p.fill();
+    };
+
+    p.preload = () => {
+      song = p.loadSound("/assets/mp3/patterns.202006.mp3");
+    };
+
+    p.setup = () => {
+      console.log("Setup");
+      p.frameRate(60);
+      const wrapper = document.querySelector("#logo-canvas-wrapper");
+      const size = wrapper.getBoundingClientRect();
+      canvas = p.createCanvas(size.width, size.height);
+      canvas.parent(wrapper);
+      brandColor = p.color(50, 48, 69);
+      backgroundColor = p.color(221, 219, 218);
+      setupParameters(10);
+      // Analysis
+      const binSize = 64;
+      fft = new p5.FFT(0.0, binSize);
+      fft.smooth(0.75);
+      sizeX = p.width / 2 / binSize;
+      octaveBands = fft.getOctaveBands(1);
+      fft.setInput(song);
+      song.play();
+    };
+
+    p.draw = () => {
+      // Fade in
+      fadeIn = p.frameCount / 100;
+      var dotColor = p.lerpColor(backgroundColor, brandColor, fadeIn);
+      // Breathing
+      let time = p.frameCount / 30.0;
+      let breathe = 0.65 + (p.sin(time) * p.cos(time) + 1.0) / 3.0;
+      let invBreathe =
+        0.75 + (1.0 - (p.sin(time) * p.cos(time) + 1.0) / 2.0) * 10.0;
+
+      // FFT
+      spectrum = fft.analyze();
+      spectrumAverages = fft.logAverages(octaveBands);
+      var spectrumAveragesRev = spectrumAverages.slice().reverse();
+      spectrumAverages = spectrumAverages.concat(spectrumAveragesRev);
+      center = fft.getEnergy("bass");
+      circleCenterRadius = p.map(
+        p.pow(center, 2),
+        0,
+        255 * 255,
+        innerRadius,
+        innerRadius * 6
+      );
+      // Clear
+      p.background(backgroundColor);
+      //
+      p.translate(p.width / 2, p.height / 2);
+      var waveRadius = outerRadius;
+      var radius = breathe * waveRadius;
+      for (var pt = 0; pt < points.length; pt++) {
+        var point = points[pt];
+        var angle = p.atan2(
+          point[1] - poissonWidth / 2,
+          point[0] - poissonWidth / 2
+        );
+        let spectralIndex = Math.floor(
+          ((((angle + time) % p.PI) + p.PI) / p.TWO_PI) *
+            spectrumAverages.length
+        );
+        var amp = spectrumAverages[spectralIndex] / 512;
+        var energy = waveRadius * amp;
+
+        var distance = p.dist(
+          poissonWidth / 2,
+          poissonWidth / 2,
+          point[0],
+          point[1]
+        );
+        if (distance > innerRadius * 40 || distance < circleCenterRadius)
+          continue;
+        var wavePoint = [
+          (radius + energy) * p.cos(angle),
+          (radius + energy) * p.sin(angle)
+        ];
+        var distanceWavePoint = p.dist(0, 0, wavePoint[0], wavePoint[1]);
+        var distancePointToWavePoint = p.dist(
+          wavePoint[0],
+          wavePoint[1],
+          point[0] - poissonWidth / 2,
+          point[1] - poissonWidth / 2
+        );
+        //// Poisson points
+        p.noStroke();
+        let circleRadius = p.map(
+          0.5 - distancePointToWavePoint / waveRadius,
+          0.3,
+          0.5,
+          0,
+          innerRadius
+        );
+        p.fill(
+          p.lerpColor(dotColor, backgroundColor, 255 - circleRadius * 255)
+        );
+        p.circle(
+          point[0] - poissonWidth / 2,
+          point[1] - poissonWidth / 2,
+          circleRadius
+        );
+      }
+      p.noStroke();
+      p.fill(dotColor);
+      p.circle(0, 0, invBreathe + circleCenterRadius);
+    };
+  }
+
+  componentDidMount() {
+    console.log("Component Did Mount");
+    if (this.props.entered === false) {
+      return;
+    }
+    if (this.sketch === undefined) {
+      this.sketch = new p5(this.Sketch, this.sketchRef.current);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <main
+          onClick={() => {
+            this.props.onEnter();
+          }}
+          className="logo-main"
+        >
+          <div
+            ref={this.sketchRef}
+            id="logo-canvas-wrapper"
+            data-song="mp3/foyer.20201025.mp3"
+          ></div>
+        </main>
+        <nav>
+          <button
+            onClick={() => {
+              this.sketch.stopAudio();
+              this.sketch.remove();
+              this.props.onBack();
+            }}
+          >
+            Zurück
+          </button>
+        </nav>
+      </>
+    );
   }
 }
 
-function switchToSong() {
-  if (mic !== undefined) mic.stop();
-  song.play();
-  fft.setInput(song);
-}
-
-function switchToMic() {
-  // Microphone input
-  if (song !== undefined) song.stop();
-  mic = new p5.AudioIn();
-  mic.start();
-  fft.setInput(mic);
-}
-
-function setup() {
-  frameRate(60);
-  const wrapper = document.querySelector("#logo-canvas-wrapper");
-  const size = wrapper.getBoundingClientRect();
-  canvas = createCanvas(size.width, size.height);
-  canvas.parent("logo-canvas-wrapper");
-  brandColor = color(50, 48, 69);
-  backgroundColor = color(241, 239, 238);
-  setupParameters(10);
-
-  // FFT
-  fft = new p5.FFT(0.0, binSize);
-  fft.smooth(0.75);
-  sizeX = width / 2 / binSize;
-  octaveBands = fft.getOctaveBands(1);
-
-  const buttonSong = document.querySelector("#button-song");
-  // const buttonSongCookie = document.querySelector("#cookie-button-song");
-  // buttonSongCookie.addEventListener("click", e => {
-  //   switchToSong();
-  // });
-  buttonSong.addEventListener("click", e => {
-    buttonMic.className = "button";
-    buttonSong.className = "button active";
-    switchToSong();
-  });
-  const buttonMic = document.querySelector("#button-mic");
-  // const buttonMicCookie = document.querySelector("#cookie-button-mic");
-  // buttonMicCookie.addEventListener("click", e => {
-  //   switchToMic();
-  // });
-  buttonMic.addEventListener("click", e => {
-    buttonSong.className = "button";
-    buttonMic.className = "button active";
-    switchToMic();
-  });
-  // document.querySelector(".buttons").style.display = "flex";
-  // document.querySelector(".audio-permission").style.opacity = 1;
-}
-
-function draw() {
-  // Fade in
-  fadeIn = 1.0; //map(frameCount, 0, 60, 0.0, 1.0);
-  var dotColor = lerpColor(backgroundColor, brandColor, fadeIn);
-  // Breathing
-  let time = frameCount / 30.0;
-  let breathe = 0.65 + (sin(time) * cos(time) + 1.0) / 3.0;
-  let invBreathe = 0.75 + (1.0 - (sin(time) * cos(time) + 1.0) / 2.0) * 10.0;
-
-  // FFT
-  spectrum = fft.analyze();
-  spectrumAverages = fft.logAverages(octaveBands);
-  var spectrumAveragesRev = spectrumAverages.slice().reverse();
-  spectrumAverages = spectrumAverages.concat(spectrumAveragesRev);
-  console.log(spectrumAverages.length);
-  center = fft.getEnergy("bass");
-  circleCenterRadius = map(
-    pow(center, 2),
-    0,
-    255 * 255,
-    innerRadius,
-    innerRadius * 6
-  );
-
-  background(backgroundColor);
-
-  //// Debugging
-  // fill(0);
-  // textAlign(LEFT);
-  // text(`Frame: ${frameCount}`, 100, 100);
-  // text(`Spectrum Bin Count: ${spectrum.length * 2}`, 100, 120);
-
-  // noFill();
-  // stroke(0);
-  // textAlign(CENTER);
-  // let baseHeight = 10;
-  // for (var i = 0; i < spectrum.length * 2; i++) {
-  //   var amp = spectrum[i];
-  //   var y = map(amp, 0, 256, 0, height / 2);
-  //   rect(
-  //     Math.log(i) * sizeX * (width / 5),
-  //     height - y - baseHeight,
-  //     sizeX * 10,
-  //     baseHeight + y
-  //   );
-  // }
-  //// Debugging
-
-  // float offsetX = 20;
-  // float offsetY = 40;
-  translate(width / 2, height / 2);
-  var waveRadius = outerRadius;
-  var radius = breathe * waveRadius;
-  for (var p = 0; p < points.length; p++) {
-    var point = points[p];
-    var angle = atan2(point[1] - poissonWidth / 2, point[0] - poissonWidth / 2);
-
-    //// Visualization description
-    // fill(0);
-    // let vizRadius = 300 - sin(((angle + PI) / TWO_PI) * 200.0) * 50;
-    // text(
-    //   `${Math.trunc(((angle + PI) * 180) / PI)}°`,
-    //   vizRadius * cos(angle),
-    //   vizRadius * sin(angle)
-    // );
-
-    let spectralIndex = Math.floor(
-      ((((angle + time) % PI) + PI) / TWO_PI) * spectrumAverages.length
-    );
-    var amp = spectrumAverages[spectralIndex] / 512;
-    var energy = waveRadius * amp;
-
-    var distance = dist(poissonWidth / 2, poissonWidth / 2, point[0], point[1]);
-    if (distance > innerRadius * 40 || distance < circleCenterRadius) continue;
-    var wavePoint = [
-      (radius + energy) * cos(angle),
-      (radius + energy) * sin(angle)
-    ];
-    var distanceWavePoint = dist(0, 0, wavePoint[0], wavePoint[1]);
-    var distancePointToWavePoint = dist(
-      wavePoint[0],
-      wavePoint[1],
-      point[0] - poissonWidth / 2,
-      point[1] - poissonWidth / 2
-    );
-
-    //// Wave
-    // fill(0, 0, 255);
-    // circle(wavePoint[0], wavePoint[1], 3);
-
-    //// Poisson points
-    noStroke();
-    let circleRadius = map(
-      0.5 - distancePointToWavePoint / waveRadius,
-      0.3,
-      0.5,
-      0,
-      innerRadius
-    );
-    fill(lerpColor(dotColor, color(241, 239, 238), 255 - circleRadius * 255));
-    circle(
-      point[0] - poissonWidth / 2,
-      point[1] - poissonWidth / 2,
-      circleRadius
-    );
-  }
-
-  noStroke();
-  fill(dotColor);
-  circle(0, 0, invBreathe + circleCenterRadius);
-}
+export default Logo;
